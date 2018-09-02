72823afd48f55ef071a5ec4334c3368525f7baaf
jtrtj <jt.r@live.com>
2018-09-02 14:59:23 -0600

    delete unused code from games controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -8,15 +8,5 @@ class Api::V1::GamesController < ApiController
     response = GameCreatorService.new(request.env["HTTP_X_API_KEY"], params[:opponent_email])
     response.run
     render json: response.game, message: response.messages
-
-    # player_1 = User.find_by(user_token: request.env["HTTP_X_API_KEY"])
-    # player_2 = User.find_by(email: params[:opponent_email])
-    # player_1_board = Board.new(4)
-    # player_2_board = Board.new(4)
-
-    # game = Game.create!(player_1_id: player_1.id,
-    #                     player_2_id: player_2.id,
-    #                     player_1_board: player_1_board,
-    #                     player_2_board: player_2_board)
   end
 end
