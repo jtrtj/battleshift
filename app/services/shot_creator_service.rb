6c227d6b095e3c66deac68a8b5523d907c32796c
jtrtj <jt.r@live.com>
2018-09-03 14:16:18 -0600

    working through refactoring shots controller

--- app/services/shot_creator_service.rb
+++ app/services/shot_creator_service.rb
@@ -1,6 +1,11 @@
 class ShotCreatorService
-  def initialize(game_id)
+  def initialize(game_id, target)
     @game_id = game_id
+    @target = target
+  end
+
+  def create_turn_process
+    TurnProcessor.new(find_game, @target)
   end
 
   def find_game
--- spec/services/shot_creator_service_spec.rb
+++ spec/services/shot_creator_service_spec.rb
@@ -5,12 +5,22 @@ describe ShotCreatorService do
     player_1 = create(:user)
     player_2 =create(:user)
     @game = create(:game, player_1: player_1, player_2: player_2)
+    @target = "A1"
+    @current_user = player_1
   end
   context 'instance methods' do
     it '#find_game' do
-      scs = ShotCreatorService.new(@game.id)
+      scs = ShotCreatorService.new(@game.id, @target)
 
       expect(scs.find_game).to eq(@game)
     end
+
+    it '#create_turn_process' do
+      scs = ShotCreatorService.new(@game.id, @target)
+
+      expect(scs.create_turn_process).to be_a(TurnProcessor)
+    end
+
+    
   end
 end
